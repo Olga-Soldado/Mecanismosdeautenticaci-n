cd8510f29a4ecb4b2f5609c5f0e11f40b6cf5863
Olga <valentina.soldado.1994@gmail.com>
2023-07-26 20:07:14 -0400

    prueba 2 modulo6

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    def authorize_request(kind = nil)
+        unless kind.include?(current_user.role)
+        redirect_to publications_path, notice: "You are not authorized to perform this action"
+        end
+    end
+    
+    protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    end
+    def after_sign_in_path_for(resource)
+        publications_path
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :publications
+  get 'home/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
